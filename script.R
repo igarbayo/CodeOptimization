45b74f1f8aff65edfd25a830c31afdf9b535d59b
igarbayo <ignacio.garbayo@rai.usc.es>
2025-04-29 20:47:53 +0200

    Gráfica Rplot01 actualizada

--- script.R
+++ script.R
@@ -6,36 +6,43 @@ head(datos)
 str(datos)
 
 # Graficar usando la función plot en R base con escala logarítmica en el eje X
-ylim_range <- range(c(datos$Q1_SIN, datos$Q3_SIN, datos$Q1_OPTIM, datos$Q3_OPTIM))
+ylim_range <- range(c(datos$Q1_SIN, datos$Q3_SIN,
+                      datos$Q1_OPT_OLD, datos$Q3_OPT_OLD,
+                      datos$Q1_OPT_NEW, datos$Q3_OPT_NEW))
 
 # Crear la gráfica con escala logarítmica en el eje X
-plot(datos$N, datos$MED_SIN, type = "n", ylim = ylim_range, xlab = "N (escala logarítmica)", ylab = "Tiempo (s)", 
-     main = "Comparación entre versión sin optimizar y optimizada", log = "x")
+plot(datos$N, datos$Q2_SIN, type = "n", ylim = ylim_range, xlab = "N (escala logarítmica)", ylab = "Tiempo (s)", 
+     main = "Comparación entre medianas y rango intercuartílico", log = "x")
 
-
-# Añadir ticks con etiquetas rotadas
-axis(1, at = datos$N, labels = FALSE)  # solo ticks
+# Añadir ticks en eje de N
+axis(1, at = datos$N, labels = FALSE)
 
 # Graficar el rango intercuartílico para la versión sin optimizar
 arrows(datos$N, datos$Q1_SIN, datos$N, datos$Q3_SIN, angle = 90, code = 3, col = "red", length = 0.1)
-# Graficar el rango intercuartílico para la versión optimizada
-arrows(datos$N, datos$Q1_OPTIM, datos$N, datos$Q3_OPTIM, angle = 90, code = 3, col = "blue", length = 0.1)
+# Graficar el rango intercuartílico para la versión optimizada old
+arrows(datos$N, datos$Q1_OPT_OLD, datos$N, datos$Q3_OPT_OLD, angle = 90, code = 3, col = "blue", length = 0.1)
+# Graficar el rango intercuartílico para la versión optimizada new
+arrows(datos$N, datos$Q1_OPT_NEW, datos$N, datos$Q3_OPT_NEW, angle = 90, code = 3, col = "green", length = 0.1)
+
 
 # Graficar las medianas
 points(datos$N, datos$Q2_SIN, col = "red", pch = 16)  # Mediana sin optimizar
-points(datos$N, datos$Q2_OPTIM, col = "blue", pch = 16)  # Mediana optimizada
+points(datos$N, datos$Q2_OPT_OLD, col = "blue", pch = 16)  # Mediana optimizada
+points(datos$N, datos$Q2_OPT_NEW, col = "green", pch = 16)  # Mediana optimizada
 
 # Unir con líneas las medianas
 lowess_sin <- lowess(datos$N, datos$Q2_SIN, f = 0.3)
 lines(lowess_sin, col = "red", lwd = 2)
-lowess_optim <- lowess(datos$N, datos$Q2_OPTIM, f = 0.3)
-lines(lowess_optim, col = "blue", lwd = 2)
+lowess_opt_old <- lowess(datos$N, datos$Q2_OPT_OLD, f = 0.3)
+lines(lowess_opt_old, col = "blue", lwd = 2)
+lowess_opt_new <- lowess(datos$N, datos$Q2_OPT_NEW, f = 0.3)
+lines(lowess_opt_new, col = "green", lwd = 2)
 
 # Añadir la leyenda
-legend("topright", 
-       legend = c("Q2_SIN", "Q2_OPTIM"), 
-       col = c("red", "blue"), 
-       pch = c(16, 16), 
-       lwd = c(2, 2), 
+legend("topleft", 
+       legend = c("SIN", "OPT_OLD", "OPT_NEW"), 
+       col = c("red", "blue", "green"), 
+       pch = c(16, 16, 16), 
+       lwd = c(2, 2, 2), 
        bty = "n", 
        inset = 0.02)
